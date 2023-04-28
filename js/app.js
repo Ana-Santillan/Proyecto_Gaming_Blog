29a29f884297ca27efcce01397340f7ca585f4bc
Mauricio <mauricioutn2017@gmail.com>
2023-04-28 02:13:28 -0300

    Corregido errores visuales

--- js/app.js
+++ js/app.js
@@ -1,28 +1,3 @@
-/*let cajaComentario = document.getElementById(`inputTexto`);
-
-cajaComentario.addEventListener("focus", habilitar);
-cajaComentario.addEventListener("blur", deshabilitar);
-
-function habilitar() {
-  let btnEnviar = document.getElementById(`botonEnviar`);
-  let btnCancelar = document.getElementById(`botonCancelar`);
-  
-  btnEnviar.className = "btn btn-primary mt-1";
-  btnCancelar.className = "btn bg-transparent mt-1";
-  
-  btnCancelar.addEventListener("mouseover", function() {
-    btnCancelar.className = "btn bg-danger mt-1";
-  });
-}
-
-function deshabilitar() {
-  let btnEnviar = document.getElementById(`botonEnviar`);
-  let btnCancelar = document.getElementById(`botonCancelar`);
-  
-  btnEnviar.className = "btn btn-secondary mt-1";
-  btnCancelar.className = "d-none";
-}
-*/
 let cajaComentario = document.getElementById(`inputTexto`);
 
 cajaComentario.addEventListener("focus",habilitar);
@@ -46,6 +21,7 @@ botonCancelar.addEventListener("click",function(){
 document.getElementById(`inputTexto`).value="";
 });
 }
+
 function deshabilitar(){
 document.getElementById(`botonEnviar`).className= "btn btn-secondary mt-1";
 document.getElementById(`botonCancelar`).className="d-none"
