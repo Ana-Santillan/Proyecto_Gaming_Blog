2488e3db15a26948a2d08d2e824d91569ec53633
Nicolas Herrera <herreranicoo17@gmail.com>
2023-04-27 23:20:31 -0300

    Agregue el texto y el boton para cerrar publicidad

--- js/publicidad.js
+++ js/publicidad.js
@@ -1,9 +1,10 @@
 function mostrarPublicidad() {
 Swal.fire({
-  html: '<a href="https://rog.asus.com/ar/" target="_blank" class="link-primary"><img src="https://dlcdnwebimgs.asus.com/gain/468AD480-354E-4AD8-93B3-59C49C65051F" class="img-fluid"></a>',
+  html: '<p class="lead">¡Aprovecha esta oportunidad ahora!</p><a href="https://rog.asus.com/ar/" target="_blank"><img src="https://dlcdnwebimgs.asus.com/gain/468AD480-354E-4AD8-93B3-59C49C65051F" class="img-fluid"></a>',
   background: "#784BA0",
-  color: "#2B86C5",
+  color: "#FF3CAC",
   timer: 5000,
+  showCloseButton: true,
   timerProgressBar: true,
   showCancelButton: false,
   showConfirmButton: false,
