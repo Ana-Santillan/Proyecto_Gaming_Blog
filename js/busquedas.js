d4e75478f6eaeaccc77348e3628eb34964d64cdc
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-04-29 19:43:25 -0300

    agrego prevent

---
+++ js/busquedas.js
@@ -0,0 +1,13 @@
+let formularioBusqueda = document.querySelector('formularioBusqueda')
+
+//manejador
+formularioBusqueda.addEventListener('submit',obtenerDatos);
+
+
+function obtenerDatos(e){
+    e.preventDefault();
+console.log('evento submit del formulari');
+let input = document.querySelector('buscador')
+console.log(typeof buscador)
+console.log(buscador.value);
+}
