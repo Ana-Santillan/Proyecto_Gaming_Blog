2794bd2de3f660579ba01c2ea52249c4e957117c
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-04-29 19:40:09 -0300

    actualizacion de rama

--- js/busquedas.js
+++ js/busquedas.js
@@ -1,1 +1,12 @@
-let formulario = document.querySelector('formularioBusqueda')
+let formularioBusqueda = document.querySelector('formularioBusqueda')
+
+//manejador
+formularioBusqueda.addEventListener('submit',obtenerDatos);
+
+
+function obtenerDatos(e){
+    e.preventDefault();
+console.log('evento submit del formulari');
+let input = document.querySelector('buscador')
+console.log(buscador.value);
+}
