01a3daa6779f68e40c0dc60ff4ffa6972a89f3b5
Mauricio <mauricioutn2017@gmail.com>
2023-05-01 20:58:45 -0300

    Agregado modulo publicidad.js

--- js/publicidad.js
+++ js/publicidad.js
@@ -1,9 +1,10 @@
 function mostrarPublicidad() {
+  console.log('hola');
 Swal.fire({
   html: '<p class="lead">¡Aprovecha esta oportunidad ahora!</p><a href="https://rog.asus.com/ar/" target="_blank"><img src="https://dlcdnwebimgs.asus.com/gain/468AD480-354E-4AD8-93B3-59C49C65051F" class="img-fluid"></a>',
   background: "#784BA0",
+  grow:"fullscreen",
   color: "#FF3CAC",
-  timer: 5000,
   showCloseButton: true,
   timerProgressBar: true,
   showCancelButton: false,
